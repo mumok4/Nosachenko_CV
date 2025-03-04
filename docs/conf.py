19750fc0c51218ed15d7d4aa486e84af82e34637
Albert Nosachenko <150217988+mumok4@users.noreply.github.com>
2025-03-04 10:46:10 +0800

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'toolchain'
+copyright = '2023, Artem Vesnin, Albert Nosachenko'
+author = 'Artem Vesnin, Albert Nosachenko'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser',
+              'sphinx.ext.mathjax']
+
+templates_path = ['_templates']
+exclude_patterns = ['projects/mtracker/*',
+                    'README.md']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# -- Options for PDF output -------------------------------------------------
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'papersize': 'a4paper',  # Формат бумаги
+    'pointsize': '10pt',     # Размер шрифта
+    'figure_align': 'htbp',  # Выравнивание изображений
+}
