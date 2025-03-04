26fa7721a9e70a437bd4c5d04ef1cfa81682ee16
Albert Nosachenko <150217988+mumok4@users.noreply.github.com>
2025-03-04 12:33:34 +0800

    Add files via upload

---
+++ docs/projects/mtracker/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+   name='mtracker',
+   version='1.0',
+   description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+   license='MIT',
+   author='Artem Vesnin',
+   coauthor = 'Albert Nosachenko',
+   author_email='artemvesnin@gmail.com',
+   coauthor_email='albert.nosachenko@gmail.com',
+   url='https://github.com/standlab/mtracker',
+   packages=['mtracker'], 
+   install_requires=[], # it is empty since we use standard python library
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
